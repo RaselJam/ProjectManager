b732872884287490f911e16ad498bb2acccc6e12
RaselJam <Rasel.jam.h@gmail.com>
2021-12-16 10:24:30 +0100

    _

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors(
   {
-    origin: 'http://localhost:3000',
+    origin: 'https://project-manager-app-russell.netlify.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 
